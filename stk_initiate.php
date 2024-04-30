0d116bc8a0485bc0fa75dfc254d17e2723d17533
Zhavia69 <62007748+Zhavia69@users.noreply.github.com>
2024-04-30 12:00:56 +0300

    second commit

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,8 +5,8 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'nk16Y74eSbTaGQgc9WF8j6FigApqOMWr'; //Fill with your app Consumer Key
-  $consumerSecret = '40fD1vRXCq90XFaU'; // Fill with your app Secret
+  $consumerKey = 'h4Vro1TaA4RPohAAKBu9ngmTA6cqwzKuRaM54U5jU4Rty9BX'; //Fill with your app Consumer Key
+  $consumerSecret = 'wKAuXkeXYW73LDujH0dKG5ENWbrrAEteArjeRFFhYpv5v2GSpUStBwAmPVVubjIH'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
